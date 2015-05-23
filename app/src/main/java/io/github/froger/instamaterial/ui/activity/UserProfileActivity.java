334cad4a68caa32089877ab52ee29d75a0810146
Edwin Anaya <edwin.anaya15@gmail.com>
2015-05-22 22:47:24 -0500

    Insta-Material
    Insta-Material v1

---
+++ app/src/main/java/io/github/froger/instamaterial/ui/activity/UserProfileActivity.java
@@ -0,0 +1,82 @@
+package io.github.froger.instamaterial.ui.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
+import android.view.ViewTreeObserver;
+
+import butterknife.InjectView;
+import io.github.froger.instamaterial.R;
+import io.github.froger.instamaterial.ui.adapter.UserProfileAdapter;
+import io.github.froger.instamaterial.ui.view.RevealBackgroundView;
+
+/**
+ * Created by Miroslaw Stanek on 14.01.15.
+ */
+public class UserProfileActivity extends BaseActivity implements RevealBackgroundView.OnStateChangeListener {
+    public static final String ARG_REVEAL_START_LOCATION = "reveal_start_location";
+
+    @InjectView(R.id.vRevealBackground)
+    RevealBackgroundView vRevealBackground;
+    @InjectView(R.id.rvUserProfile)
+    RecyclerView rvUserProfile;
+
+    private UserProfileAdapter userPhotosAdapter;
+
+    public static void startUserProfileFromLocation(int[] startingLocation, Activity startingActivity) {
+        Intent intent = new Intent(startingActivity, UserProfileActivity.class);
+        intent.putExtra(ARG_REVEAL_START_LOCATION, startingLocation);
+        startingActivity.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+        setupUserProfileGrid();
+        setupRevealBackground(savedInstanceState);
+    }
+
+    private void setupRevealBackground(Bundle savedInstanceState) {
+        vRevealBackground.setOnStateChangeListener(this);
+        if (savedInstanceState == null) {
+            final int[] startingLocation = getIntent().getIntArrayExtra(ARG_REVEAL_START_LOCATION);
+            vRevealBackground.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+                @Override
+                public boolean onPreDraw() {
+                    vRevealBackground.getViewTreeObserver().removeOnPreDrawListener(this);
+                    vRevealBackground.startFromLocation(startingLocation);
+                    return true;
+                }
+            });
+        } else {
+            vRevealBackground.setToFinishedFrame();
+            userPhotosAdapter.setLockedAnimations(true);
+        }
+    }
+
+    private void setupUserProfileGrid() {
+        final StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
+        rvUserProfile.setLayoutManager(layoutManager);
+        rvUserProfile.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                userPhotosAdapter.setLockedAnimations(true);
+            }
+        });
+    }
+
+    @Override
+    public void onStateChange(int state) {
+        if (RevealBackgroundView.STATE_FINISHED == state) {
+            rvUserProfile.setVisibility(View.VISIBLE);
+            userPhotosAdapter = new UserProfileAdapter(this);
+            rvUserProfile.setAdapter(userPhotosAdapter);
+        } else {
+            rvUserProfile.setVisibility(View.INVISIBLE);
+        }
+    }
+}
